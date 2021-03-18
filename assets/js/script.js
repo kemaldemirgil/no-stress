211f3ad185588d2aaa45d89134dce15efa1c857d
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-03-17 21:40:15 -0400

    adding localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 let currentDay = $("#currentDay");
+let textArea = $(".textarea");
+let listArray = [];
+var myVal;
+
 updateCurrentTime();
 setInterval(updateCurrentTime, 1000);
 
@@ -6,7 +10,7 @@ function updateCurrentTime() {
     currentDay.text(moment().format('MMMM Do YYYY, h:mm:ss a')); // March 16th 2021, 10:42:35 am
 }
 
-$(".textarea").each(function(number, item) {
+$(textArea).each(function(number, item) {
     if (parseInt($(item).data("number")) < moment().hour()) {
         $(item).attr("class", "past");
     } else if (parseInt($(item).data("number")) > moment().hour()) {
@@ -16,3 +20,29 @@ $(".textarea").each(function(number, item) {
     }
     // console.log(item);
 })
+
+// $.each(textArea, function () {
+//     var myVal = localStorage.setItem($(textArea).val(), JSON.stringify());
+//     listArray.push(myVal);
+// });
+// console.log(listArray);
+// console.log(localStorage);
+// $.each(textArea, function() {
+//     var myVal = $("textarea").val();
+//     console.log(myVal);
+// });
+
+// $(textArea).each(function(item) {
+//     myVal = localStorage.setItem("listArray", $(item).val());
+//     // console.log(myVal);
+//     // console.log(listArray);
+//     console.log(localStorage);
+//     // console.log(myVal);
+// })
+$(textArea).each(function(index, element) {
+    var textValue = $(element).val();
+    listArray.push(textValue);
+    localStorage.setItem(index, textValue);
+    textArea = localStorage.getItem(listArray);
+})
+console.log(localStorage);
